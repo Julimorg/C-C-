0d0fa3ecd3ba866db269b1d40147508d6ce89120
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-09-21 13:17:41 +0700

    init

---
+++ Basic/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by acer on 9/21/2025.
+//
+
+#include "HelloWorld.h"
+#include "iostream"
+
+int main(){
+    printf("Hello World!");
+
+    return 0;
+}
---
+++ Basic/HelloWorld.h
@@ -0,0 +1,14 @@
+//
+// Created by acer on 9/21/2025.
+//
+
+#ifndef CPP_AND_C_HELLOWORLD_H
+#define CPP_AND_C_HELLOWORLD_H
+
+
+class HelloWorld {
+
+};
+
+
+#endif //CPP_AND_C_HELLOWORLD_H
