36175bccd202b7c1a878e351a6913895b5a1d1fd
Julimorg <kienphongtran2003@gmail.com>
2026-02-05 22:29:14 +0700

    thank

--- DataStructureAndAlgorithm/Recursion/Fibonacci_1.cpp
+++ DataStructureAndAlgorithm/Recursion/Fibonacci_1.cpp
@@ -7,10 +7,26 @@
 
 using namespace std;
 
+// O ( 2 ^ N )
 int fib(int n ) {
     if ( n <= 1) return n;
     // cout << "Print Fib: " << n << endl;
     return fib(n - 1) + fib(n - 2);
+}
+int fun (int n)
+
+{
+
+    int x=1, k;
+
+    if (n==1) return x;
+
+    for (k=1; k<n; ++k)
+
+        x=x + fun (k) * fun (n - k);
+
+    return x;
+
 }
 int fibonacci(int n ) {
     if ( n  <= 1) return n;
@@ -28,14 +44,35 @@ int fibonacci(int n ) {
     return s;
 }
 
+void count(int n)
+
+{
+
+    static int d = 1;
+
+    printf("%d", n);
+
+    printf("%d", d);
+
+    d++;
+
+    if (n > 1) count (n-1);
+
+    printf("%d", d);
+
+}
+
 int main() {
 
     cout << "FIB: " << fibonacci(7) ;
     cout << endl;
-    for (int i = 0; i <= 7; i++) {
-        printf("F(%d) = %d\n", i, fib(i));
-    }
+    // for (int i = 0; i <= 50; i++) {
+    //     printf("F(%d) = %d\n", i, fib(i));
+    // }
+    cout << fun(5);
 
+    cout <<  endl;
+    count (3);
 
     return 0;
 
