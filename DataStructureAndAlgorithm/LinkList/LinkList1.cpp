01ab318abaf76adecc9fc45039707d758cbf8e08
Julimorg <kienphongtran2003@gmail.com>
2026-02-28 09:52:09 +0700

    nah

--- DataStructureAndAlgorithm/LinkList/LinkList1.cpp
+++ DataStructureAndAlgorithm/LinkList/LinkList1.cpp
@@ -25,6 +25,87 @@ void insertHead(Node *&head, Node *newNode) {
     head =  newNode;
 }
 
+void insertTail(Node *&head, Node *newNode) {
+    Node *p = head;
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    while ( p->next != NULL) {
+        p = p->next;
+    }
+    p->next = newNode;
+
+}
+
+void insetIntoPos(Node *&head,  Node *newNode, int index) {
+
+    if ( head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node *p = head;
+
+    for ( int i = 0 ; i < index - 1; i++) {
+        p = p->next;
+    }
+
+    newNode->next = p->next;
+
+    p->next = newNode;
+}
+
+void deleteHead (Node* &head) {
+    Node *p = head;
+    head = head->next;
+    delete p;
+}
+
+void deleteTail(Node *&head) {
+
+    if (head == NULL) {
+        cout << "Empty List" << endl;
+        return;
+    }
+
+    if (head->next == NULL) {
+        delete head;
+        head = NULL;
+        return;
+    }
+
+    Node *p = head;
+
+    while ( p->next->next != NULL) {
+        p = p->next;
+    }
+    Node *nodeToDelete = p->next;
+
+    p->next = NULL;
+
+    delete nodeToDelete;
+}
+void deleteNodeAtIndex( Node* &head, int index) {
+
+    if (head == NULL ) {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    Node *p = head;
+    for ( int i = 0 ; i < index - 1 ; i++) {
+        p = p->next;
+    }
+
+    Node *nodeToDelete = p->next;
+
+    p->next = nodeToDelete->next;
+
+    delete nodeToDelete;
+}
 void countNode( Node *head) {
     int count = 0;
     cout << "the number of node in linklist is: " ;
@@ -38,12 +119,11 @@ void countNode( Node *head) {
 void printList(Node *head) {
     cout << "LINK LIST: " << endl;
     Node *node = head;
-    while ( node->next != NULL) {
+    while ( node != NULL) {
         cout << node->data << " -> " ;
-
         node = node->next;
-
     }
+
     cout << " NULL " << endl;
 }
 
@@ -54,9 +134,27 @@ int main() {
     insertHead(head, createNewNode(30));
     insertHead(head, createNewNode(40));
     insertHead(head, createNewNode(50));
-
+    insertTail(head, createNewNode(100));
+    insetIntoPos(head, createNewNode(200), 3);
     printList(head);
     countNode(head);
+
+    deleteHead(head);
+
+    cout << "\n ================== " << endl;
+    cout << " List after delete head : " << endl ;
+    printList(head);
+
+    deleteTail(head);
+    cout << "\n ================== " << endl;
+    cout << " List after delete tail : " << endl ;
+    printList(head);
+
+    deleteNodeAtIndex(head, 2);
+    cout << "\n ================== " << endl;
+    cout << " List after delete at position : " << endl ;
+    printList(head);
+
 
     return 0;
 }
