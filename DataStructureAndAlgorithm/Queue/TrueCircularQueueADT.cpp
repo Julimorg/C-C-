d954283b3311f95053215e56788e94307cf6a985
Julimorg <kienphongtran2003@gmail.com>
2026-03-07 15:02:50 +0700

    dq

--- DataStructureAndAlgorithm/Queue/TrueCircularQueueADT.cpp
+++ DataStructureAndAlgorithm/Queue/TrueCircularQueueADT.cpp
@@ -1,68 +1,63 @@
 //
-// Created by jianf on 3/5/2026.
+// Created by jianf on 3/7/2026.
 //
 
-#include "Fix_For_TranKienPhongPractice2A.h"
+#include "TrueCircularQueueADT.h"
 #include "iostream"
-
 using namespace std;
 #define MAX_QUEUE 10
-
 struct Queue {
     int A[MAX_QUEUE];
     int front;
     int rear;
+    int count;
 };
-
 void initQueue(Queue *&q) {
-    q->front = -1;
-    q->rear = -1;
+    q->front = 0;
+    q->rear = 0;
+    q->count = 0;
 }
-
-bool isFull(Queue *q) {
-    return (q->rear + 1) % MAX_QUEUE == q->front;
-}
-
 bool isEmpty(Queue *q) {
-    return q->front == -1;
+    return q->count == 0;
+}
+bool isFull(Queue *q) {
+    return q->count == MAX_QUEUE;
 }
 
 void enqueue(Queue *&q, int data) {
     if (isFull(q)) {
-        cout << "The Queue is full!" << endl;
+        cout << "The queue is full!" << endl;
         return;
     }
-    if (isEmpty(q)) {
-        q->front = 0;
-    }
-    q->rear =  ( q->rear + 1 ) % MAX_QUEUE;
-    q->A[q->rear] = data;
-    cout << "\nEnqueue: " << data << " at rear :" << q->rear << endl;
-}
 
-int dequeue(Queue *q) {
+
+    q->A[q->rear] = data;
+    cout << "\nEnqueue: " << data << " at rear : " << q->rear << endl;
+    q->rear = ( q->rear + 1 ) % MAX_QUEUE;
+    q->count++;
+}
+int dequeue(Queue *&q) {
     if (isEmpty(q)) {
         cout << "The Queue is empty!" << endl;
+        return -1;
     }
     int data = q->A[q->front];
-    if (q->front ==  q->rear) {
-        q->rear = -1;
-        q->front = -1;
-    }else {
-        q->front = ( q->front + 1 ) % MAX_QUEUE;
-    }
-    return data;
+    q->front = ( q->front + 1 ) % MAX_QUEUE;
+    q->count--;
+   return data;
 }
 int count(Queue *q) {
-    return q->rear - q->front;
+    return q->count;
 }
+
 void printQueue(Queue *q) {
-    cout << "Queue: " << endl;
-    cout << "front: " << q->front << " <<<< rear :" << q->rear << endl;
-    for (int i = q->front ;  i <= q->rear ; i++) {
+    cout << "The queue is: " << endl;
+    int i = q->front;
+    for ( int k = 0 ; k < q->count ; k++) {
         cout << q->A[i] << " --> " ;
+        i =  ( i + 1 ) % MAX_QUEUE;
     }
-    cout << "\nCount : " << count(q);
+    cout << "\n Queue has: " << count(q) << " items " << endl;
 }
 
 int main() {
@@ -101,6 +96,4 @@ int main() {
 
 
     printQueue(q);
-
-
 }
---
+++ DataStructureAndAlgorithm/Queue/TrueCircularQueueADT.h
@@ -0,0 +1,13 @@
+//
+// Created by jianf on 3/7/2026.
+//
+
+#ifndef CPP_AND_C___TRUECIRCULARQUEUEADT_H
+#define CPP_AND_C___TRUECIRCULARQUEUEADT_H
+
+
+class TrueCircularQueueADT {
+};
+
+
+#endif //CPP_AND_C___TRUECIRCULARQUEUEADT_H
