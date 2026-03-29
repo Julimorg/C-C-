aadee4c1730ea9a4657aac6c26af22b7f0f3b6a0
Julimorg <kienphongtran2003@gmail.com>
2026-03-29 13:58:24 +0700

    dwq

--- DataStructureAndAlgorithm/Practice/PracticeTree1.cpp
+++ DataStructureAndAlgorithm/Practice/PracticeTree1.cpp
@@ -1,10 +1,7 @@
-//
-// Created by jianf on 3/28/2026.
-//
-
-#include "PracticeTree1.h"
+#include "stdlib.h"
 #include "iostream"
-using namespace  std;
+
+using namespace std;
 
 struct Node {
     int data;
@@ -14,112 +11,129 @@ struct Node {
 
 Node *createNewNode(int data) {
     Node *newNode = new Node;
-    newNode->left = NULL;
-    newNode->right = NULL;
     newNode->data = data;
+    newNode->right = NULL;
+    newNode->left = NULL;
     return newNode;
 }
 
-Node * insertNode(Node *root, int data) {
-    if (root == NULL) {
+Node *findMin(Node *root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node *insertNode(Node *root, int data) {
+    if ( root == NULL) {
         return createNewNode(data);
     }
 
-    if (root->data > data) {
+    if ( root->data > data) {
         root->left = insertNode(root->left, data);
-    }else if (root->data < data) {
-         root->right = insertNode(root->right, data);
+    }else if ( root->data < data) {
+        root->right = insertNode(root->right, data);
     }
 
     return root;
 }
-Node* findMin(Node* root) {
-    while (root->left != NULL)
-        root = root->left;
-    return root;
-}
 
-Node *deleteNode(Node *root, int key) {
+Node *deleteNode (Node *root, int data) {
     if (root == NULL) return NULL;
 
-    if (key < root->data) {
-        root->left = deleteNode(root->left, key);
-    }
-    else if (key > root->data) {
-        root->right = deleteNode(root->right, key);
-    }
-    else {
-        // Case 1 & 2: 0 hoặc 1 con
-        if (root->left == NULL) {
-            Node* temp = root->right;
-            delete root;
-            return temp;
+    if (root->data > data) {
+        root->left = deleteNode(root->left, data);
+    }else if (root->data < data) {
+        root->right = deleteNode (root->right , data);
+    }else {
+        if ( root->right == NULL) {
+            Node *temp = root;
+            root = root->left;
+            delete(temp);
+            return root;
+        }else if ( root->left == NULL) {
+            Node *temp = root;
+            root = root->right ;
+            delete(temp);
+            return root;
         }
-        else if (root->right == NULL) {
-            Node* temp = root->left;
-            delete root;
-            return temp;
-        }
-
-        // Case 3: 2 con
-        Node* temp = findMin(root->right);
-
-        root->data = temp->data;
-
-        root->right = deleteNode(root->right, temp->data);
+        Node *successor = findMin(root->right);
+        root->data = successor->data;
+        root->right = deleteNode(root->right, successor->data);
     }
-
     return root;
 }
-void preOrder(Node *root) {
-    if (root == NULL ) return ;
 
+void preOrder(Node *root) {
+    if ( root == NULL) return ;
     cout << root->data << " --> ";
     preOrder(root->left);
     preOrder(root->right);
 }
 
 void infixOrder(Node *root) {
-
-    if (root == NULL) return;
-
+    if ( root == NULL) return ;
     infixOrder(root->left);
-
     cout << root->data << " --> ";
-
     infixOrder(root->right);
+}
 
+void postOrder(Node *root) {
+    if ( root == NULL) return ;
+    infixOrder(root->left);
+    infixOrder(root->right);
+    cout << root->data << " --> ";
 }
 
 int main() {
     Node *root = NULL;
-
     root = insertNode(root, 10);
     root = insertNode(root, 5);
-    root = insertNode(root, 7);
+    root = insertNode(root, 20);
     root = insertNode(root, 3);
+    root = insertNode(root, 8);
     root = insertNode(root, 15);
+    root = insertNode(root, 25);
+    root = insertNode(root, 16);
 
-    cout << "\nInfix Order: " << endl;
-    infixOrder(root);
-    cout << " \n ---- \n" ;
+    printf ("\n------\n");
 
-    cout << "PreOrder: \n" << endl;
-    preOrder(root);
-
-    cout << " \n ---- \n" ;
-
-    cout << "After delete\n " << endl;
-    deleteNode(root, 10) ;
-
-    cout << "\nInfix Order: " << endl;
-    infixOrder(root);
-    cout << " \n ---- \n" ;
-
-    cout << "PreOrder: \n" << endl;
+    printf ("Pre Order : \n" );
     preOrder(root);
 
 
+    printf ("\n------\n");
 
-    return 1;
+    printf ("Infix Order : \n" );
+    infixOrder(root);
+
+
+    printf ("\n------\n");
+
+    printf ("Post Order : \n" );
+    postOrder(root);
+
+
+    printf ("\n ---------- DELTE NODE ---------- \n");
+    root = deleteNode(root, 10);
+
+
+    printf ("Pre Order : \n" );
+    preOrder(root);
+
+
+    printf ("\n------\n");
+
+    printf ("Infix Order : \n" );
+    infixOrder(root);
+
+
+    printf ("\n------\n");
+
+    printf ("Post Order : \n" );
+    postOrder(root);
+
+
+
+
 }
